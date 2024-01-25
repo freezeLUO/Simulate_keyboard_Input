0755fec2c8ab6a58a44643bcdb8a99bbb36ed916
freezeLUO <44939613+freezeLUO@users.noreply.github.com>
2024-01-25 18:53:12 +0800

    Update Simulate-keyboard-Input（模拟键盘输入）.py

--- Simulate-keyboard-Input（模拟键盘输入）.py
+++ Simulate-keyboard-Input（模拟键盘输入）.py
@@ -2,33 +2,39 @@
 import tkinter as tk
 import pynput
 import time
-
 # 创建一个窗口
 window = tk.Tk()
-window.title('模拟输入GUI(延迟3s后开始输入)')
+window.title('模拟输入GUI')
 window.geometry('450x300')
 
-# 创建一个文本框
 text = tk.Text(window, wrap=tk.WORD, width=20, height=5)
 text.pack(expand=True, fill=tk.BOTH)
-
 # 创建一个函数来实现模拟输入的功能
 def simulate_input():
     # 获取文本框中的内容
     text_content = text.get('1.0', 'end')
-
     keyboard = pynput.keyboard.Controller()
 
-    time.sleep(3)
+    delay_3 = scale_3.get()
+    # 延迟用户选择的时间
+    time.sleep(delay_3)
 
-    #keyboard.type(text_content)
+    delay_005 = scale_005.get()
+    # 模拟键盘输入字符串，每个字符之间延迟用户选择的时间
     for char in text_content:
         keyboard.type(char)
-        time.sleep(0.05)  
-
+        time.sleep(delay_005)  
 
 button = tk.Button(window, text='确认', command=simulate_input)
 button.pack()
+# 创建一个Scale组件，用于选择开始输入的延迟的时间
+scale_3 = tk.Scale(window, from_=0, to=20, orient=tk.HORIZONTAL, label='开始输入的延迟', length=200)
+scale_3.pack()
+# 创建一个Scale组件，用于选择输入字符的间隔延迟秒的时间
+scale_005 = tk.Scale(window, from_=0.01, to=1, orient=tk.HORIZONTAL, label='输入字符的间隔', resolution=0.01, length=200)
+scale_005.pack()
+# 运行窗口
 window.mainloop()
+
 
 
